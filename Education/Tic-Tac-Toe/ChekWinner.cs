1a9bc1f0bad14249069eb7c84e0079d89e386dec
Nfernal45 <nfernal@yandex.ru>
2022-07-26 17:16:08 +0400

    Work Verion Tic-Tac-Toe

--- Education/Tic-Tac-Toe/ChekWinner.cs
+++ Education/Tic-Tac-Toe/ChekWinner.cs
@@ -3,81 +3,119 @@ namespace Tic_Tac_Toe;
 
 public class ChekWin
 {
-		public static void ChekWinner3(int x, int y)
+    public static void ChekWinner3(int x, int y)
+    {
+
+           ChekHorizontal(x, y);
+        if (Program.countStep > Array.array.Length - 1)
+            ChekDraw();
+        else Program.TestGame();
+    }
+
+    static void ChekHorizontal(int x, int y)
+    {
+
+        int count = 0;
+
+        for (int i = 0; i < Program.arrayY && count < 3; i++)
+            if (Array.array[x, i] == Program.step)
+                count++;
+            else count = 0;
+        if (count == 3)
+            ViewWinner.Winner();
+
+        ChekVertical(x, y);
+
+    }
+
+    static void ChekVertical(int x, int y)
+    {
+
+        int count = 0;
+
+        for (int i = 0; i < Program.arrayY; i++)
+            if (Array.array[i, y] == Program.step)
+            {
+                count++;
+                if (count == 3)
+                    ViewWinner.Winner();
+            }
+            else count = 0;
+
+        ChekCornerLeft(x, y);
+
+    }
+
+    static void ChekCornerLeft(int x, int y)
+    {
+        int count = 0;
+
+        for (int i = x, j = y; i > -1 && j > -1; i--, j--)
         {
-        Chek1(x,y);
-        Program.TestGame();
+            if (Array.array[i, j] == Program.step)
+            {
+                count++;
+                if (count == 3)
+                    ViewWinner.Winner();
+            }
+            else count = 0;
+
+        }
+        count = 0;
+        for (int i = x, j = y; i != Program.arrayX && j != Program.arrayY; i++, j++)
+        {
+            if (Array.array[i, j] == Program.step)
+            {
+                count++;
+                if (count == 3)
+                    ViewWinner.Winner();
+            }
+            else count = 0;
+
         }
 
-	static void Chek1(int x, int y) 
-    {
-        int a;
-        int b;
-            a = x - 1;
-            b = x + 1;
 
-            if (Array.array[a,y] == Program.step && Array.array[b, y] == Program.step)
-            ViewWinner.Winner();
-        else Chek2(x, y);
-   // Вызываем метод отображения победы
+        ChekCornerRight(x, y);
+
     }
 
-    static void Chek2(int x, int y)
+    static void ChekCornerRight(int x, int y)
     {
-        int a = x - 1;
-        int b = y + 1;
         int count = 0;
-        if (Array.array[x, a] == Program.step)
-            count++;
-        else Chek3(x, y);
 
-        if (Array.array[x, b] == Program.step)
-            count++;
-        else Chek3(x, y);
+        for (int i = x, j = y; i > -1 && j != Program.arrayY; i--, j++)
+        {
+            if (Array.array[i, j] == Program.step)
+            {
+                count++;
+                if (count == 3)
+                    ViewWinner.Winner();
+            }
+            else count = 0;
 
-        if (count == 2)
-            ViewWinner.Winner();
-        // Вызываем метод отображения победы
+        }
+        count = 0;
+        for (int i = x, j = y;i != Program.arrayX && j > -1; i++, j--)
+        {
+            if (Array.array[i, j] == Program.step)
+            {
+                count++;
+                if (count == 3)
+                    ViewWinner.Winner();
+            }
+            else count = 0;
+
+        }
+
     }
-
-    static void Chek3(int x, int y)
+    static void ChekDraw()
     {
-        int a = x - 1;
-        int b = y - 1;
-        int count = 0;
-        if (Array.array[a, b] == Program.step)
-            count++;
-        else Chek4(x, y);
-
-        a = x + 1;
-        b = y + 1;
-        if (Array.array[a, b] == Program.step)
-            count++;
-        else Chek4(x, y);
-
-        if (count == 2)
-            ViewWinner.Winner();
-        // Вызываем метод отображения победы
-    }
-
-    static void Chek4(int x, int y)
-    {
-        int a = x - 1;
-        int b = y + 1;
-        int count = 0;
-        if (Array.array[a, b] == Program.step)
-            count++;
-        //else Chek5(x, y);
-
-        a = x + 1;
-        b = y - 1;
-        if (Array.array[a, b] == Program.step)
-            count++;
-       //else Chek5(x, y);
-
-        if (count == 2)
-            ViewWinner.Winner();
-        // Вызываем метод отображения победы
+        Console.Clear();
+        Array.ArrayGame(Program.arrayX, Program.arrayY);
+        Console.WriteLine();
+        Console.WriteLine("Ничья");
+        Console.WriteLine();
+        Program.NewGame();
     }
 
 }
--- Education/Tic-Tac-Toe/Program.cs
+++ Education/Tic-Tac-Toe/Program.cs
@@ -2,36 +2,36 @@
 {
     internal class Program
     {
-        public static int ff;
+        public static int countStep;
         public static String step = "X";
         static int countNewGame = 0;
         public static int arrayX;
         public static int arrayY;
 
-        static void Main(string[] args)
+        static void Main()
         {
-            FieldX(true);
+            InputFieldX(true);
             TestGame();
 
         }
        public static void TestGame()
         {
-            Console.Clear();
-            Console.WriteLine("Welcome to the Super Duper Tic Tac Toe Game!");
+            Console.Clear();           
+            Console.WriteLine();
             Console.WriteLine("Player 1: X");
             Console.WriteLine("Player 2: O");
             Console.WriteLine();
-            if ((ff % 2) == 0)
+            if ((countStep % 2) == 0)
                 Console.WriteLine($"Player 1 to move, select 1 thorugh {Array.array.Length} from the game board.");
             else
                 Console.WriteLine($"Player 2 to move, select 1 thorugh {Array.array.Length} from the game board.");
-            ff++;
+            countStep++;
             Console.WriteLine();
             Array.ArrayGame(arrayX, arrayY);
             Input();
 
         }
-        static void Input()                 //Ввод с консоли и получение координат массива
+        static void Input() 
         {
             int x = 0;
             int y = 0;
@@ -52,7 +52,7 @@
                     }
                     x = i;
                 }
-                ChekInput(x, y);             //Вызываем метод проверки
+                ChekInput(x, y); 
             }
             else
             {
@@ -61,7 +61,7 @@
             }
         }
 
-        static void ChekInput(int x, int y) //Проверка элемента массива на уже имеющийся ход  
+        static void ChekInput(int x, int y) 
         {
             if (Array.array[x, y] == "X" || Array.array[x, y] == "O")
             {
@@ -69,12 +69,12 @@
                 Input();
             }
             else
-                SetStep(x, y);              //Если все ок вызываем метод записи хода в массив
+                SetStep(x, y);              
 
         }
-        static void SetStep(int x, int y)   //Записываем ход в массив, мы молодцы
+        static void SetStep(int x, int y)   
         {
-            if ((ff % 2) == 0)
+            if ((countStep % 2) == 0)
             {
                 Array.array[x, y] = "O";
                 step = "O";
@@ -84,49 +84,11 @@
                 Array.array[x, y] = "X";
                 step = "X";
             }
-            if (arrayX == 3 && arrayY == 3)
-                ChekWinner();
-            else ChekWin.ChekWinner3(x, y);
-
-        }
-
-        static void ChekWinner()
-        {
-            if (ff > 4)
-            {
-                if ((Array.array[0, 0] == step && Array.array[0, 1] == step && Array.array[0, 2] == step) ||
-                    (Array.array[1, 0] == step && Array.array[1, 1] == step && Array.array[1, 2] == step) ||
-                    (Array.array[2, 0] == step && Array.array[2, 1] == step && Array.array[2, 2] == step) ||
-                    (Array.array[0, 0] == step && Array.array[1, 0] == step && Array.array[2, 0] == step) ||
-                    (Array.array[0, 1] == step && Array.array[1, 1] == step && Array.array[2, 1] == step) ||
-                    (Array.array[0, 2] == step && Array.array[1, 2] == step && Array.array[2, 2] == step) ||
-                    (Array.array[0, 2] == step && Array.array[1, 1] == step && Array.array[2, 0] == step) ||
-                    (Array.array[0, 0] == step && Array.array[1, 1] == step && Array.array[2, 2] == step))
-                {
-                    Console.Clear();
-                    Array.ArrayGame(arrayX, arrayY);
-                    Console.WriteLine();
-                    Console.WriteLine($"{step} победил");
-                    Console.WriteLine();
-                    NewGame();
-                }
-                else if (ff < Array.array.Length) TestGame();
-
-                else
-                {
-                    Console.Clear();
-                    Array.ArrayGame(arrayX, arrayY);
-                    Console.WriteLine();
-                    Console.WriteLine("Ничья");
-                    Console.WriteLine();
-                    NewGame();
-                }
-            }
+             if (countStep > 4)
+                ChekWin.ChekWinner3(x, y);
             else TestGame();
 
-
         }
-
         public static void NewGame()
         {
             if (countNewGame == 0)
@@ -140,7 +102,7 @@
             string game = Console.ReadLine();
             if (game == "y")
             {
-                ff = 0;
+                countStep = 0;
                 step = "X";
                 Array.firstout = 0;
                 TestGame();
@@ -148,8 +110,10 @@
             else if (game == "n")
             {
                 Console.Clear();
-                Console.WriteLine("\"Спасибо Клим Саныч");
-                Console.WriteLine(" Честно говоря потраченного времени жаль...\"");
+                Console.WriteLine("\"Я в своем познании настолько преисполнился,");
+                Console.WriteLine(" что я как будто бы уже сто триллионов миллиардов лет проживаю ");
+                Console.WriteLine(" на триллионах и триллионах таких же планет, как эта Земля...\"");
+                Environment.Exit(0);
             }
 
             else
@@ -160,15 +124,14 @@
                 NewGame();
             }
 
-
-
-
         }
 
-        static void FieldX(Boolean rt) //Еще пока не дописал
+        static void InputFieldX(Boolean rt) //Еще пока не дописал
         {
             if (rt)
             {
+                Console.WriteLine("Welcome to the Super Duper Tic Tac Toe Game!");
+                Console.WriteLine();
                 Console.WriteLine("Введи размеры поля. Сначала кол-во столбоцов, потом кол-во строк");
                 Console.WriteLine();
                 Console.WriteLine("Поле должно быть не менее 3х3");
@@ -177,24 +140,21 @@
             }
             string? stolb = Console.ReadLine();
 
-
-
             if (int.TryParse(stolb, out int v))
             {
                 if (v < 3)
                 {
                     Console.WriteLine("ПОЛЕ.ДОЛЖНО.БЫТЬ.НЕ.МЕНЕЕ.3х3!!!");
-                    FieldX(false);
+                    InputFieldX(false);
                 }
                 else
                 {
                     Console.WriteLine(v);
                     arrayX = v;
                     Console.Clear();
-                    FieldY(true);
+                    InputFieldY(true);
                 }
             }
-
 
             else
             {
@@ -202,18 +162,17 @@
                 Console.WriteLine($"Разве {stolb} это цифра?");
                 Console.WriteLine();
                 Console.WriteLine("Попытайся еще раз ввести количество столбцов, на этот раз не промахнись");
-                FieldX(false);
+                InputFieldX(false);
                 Console.Clear();
             }
 
-
         }
 
-        static void FieldY(Boolean rt) //Еще пока не дописал
+        static void InputFieldY(Boolean rt) //Еще пока не дописал
         {
             if (rt)
             {
-                Console.WriteLine("Введи размеры поля. Сначала кол-во столбоцов, потом кол-во строк");
+                Console.WriteLine("Welcome to the Super Duper Tic Tac Toe Game!");
                 Console.WriteLine();
                 Console.WriteLine("Поле должно быть не менее 3х3");
 
@@ -221,14 +180,12 @@
             }
             string? stolb = Console.ReadLine();
 
-
-
             if (int.TryParse(stolb, out int v))
             {
                 if (v < 3)
                 {
                     Console.WriteLine("ПОЛЕ.ДОЛЖНО.БЫТЬ.НЕ.МЕНЕЕ.3х3!!!");
-                    FieldX(false);
+                    InputFieldX(false);
                 }
                 else
                 {
@@ -243,19 +200,9 @@
                 Console.WriteLine($"Разве {stolb} это цифра?");
                 Console.WriteLine();
                 Console.WriteLine("Попытайся еще раз ввести количество столбцов, на этот раз не промахнись");
-                FieldY(false);
+                InputFieldY(false);
                 Console.Clear();
             }
-
-
-        }
-        static void ChekWinner2()
-        {
-            int r = 0;
-            int t = 0;
-            int s = 1;
-
-
         }
     }
 }
